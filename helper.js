cd6b19dafd99eeace08546af166cdc868bafd73a
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2021-02-02 21:22:44 +0000

    helper: add ws url port for now

--- helper.js
+++ helper.js
@@ -2,8 +2,9 @@ export const stringContains = (str = '', ...terms) => RegExp(terms.join('|')).te
 
 export const resolveLocalAddress = (url = '') => {
   const { origin } = location
+  const { port } = new URL(url)
 
-  return stringContains(origin, 'local', 'test') ? url : origin
+  return stringContains(origin, 'local', 'test') ? url : `${origin}:${port}`
 }
 
 export const websocketAddressFrom = (url = '') => resolveLocalAddress(url).replace(/^http/, 'ws')
